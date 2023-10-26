eb40925a80b7085d6285780f1c550c7c8df9eb9a
JordiMilian <51137598+JordiMilian@users.noreply.github.com>
2023-10-27 00:22:45 +0200

    GItHubTest
    test

--- Assets/Scripts/GitHubTest.cs
+++ Assets/Scripts/GitHubTest.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Diagnostics;
 using UnityEngine;
 
 public class GitHubTest : MonoBehaviour
@@ -7,6 +9,11 @@ public class GitHubTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        TestingGit();
+    }
+    void TestingGit()
+    {
+        Debug.Log("Testgit");
         
     }
 
