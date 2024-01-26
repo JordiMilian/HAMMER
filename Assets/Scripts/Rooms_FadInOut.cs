bd77f1bee22bf2351afebe4b8d8e1cc67becdfb5
JordiMilian <51137598+JordiMilian@users.noreply.github.com>
2024-01-26 17:54:46 +0100

    RoomFader

---
+++ Assets/Scripts/Rooms_FadInOut.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Generic_OnTriggerEnterEvents;
+
+
+public class Rooms_FadInOut : MonoBehaviour
+{
+    SpriteRenderer[] bottomRoomSprites = new SpriteRenderer[0];
+    SpriteRenderer[] topRoomSprites = new SpriteRenderer[0];
+    [SerializeField] Generic_OnTriggerEnterEvents topTrigger;
+    [SerializeField] Generic_OnTriggerEnterEvents bottomTrigger;
+    public GameObject TopRoom;
+    public GameObject BottomRoom;
+    [SerializeField] float TransitionTime = 0.2f;
+    private void Awake()
+    {
+        bottomRoomSprites = BottomRoom.GetComponentsInChildren<SpriteRenderer>();
+        topRoomSprites = TopRoom.GetComponentsInChildren<SpriteRenderer>();
+    }
+    private void OnEnable()
+    {
+        topTrigger.ActivatorTags.Add("Player_SinglePointCollider");
+        topTrigger.OnTriggerEntered += playerEnteredTop;
+
+        bottomTrigger.ActivatorTags.Add("Player_SinglePointCollider");
+        bottomTrigger.OnTriggerEntered += playerEnteredBottom;
+    }
+    private void OnDisable()
+    {
+        topTrigger.OnTriggerEntered -= playerEnteredTop;
+        bottomTrigger.OnTriggerEntered -= playerEnteredBottom;
+    }
+    void playerEnteredTop(object sender, EventArgsTriggererInfo args)
+    {
+        topTrigger.gameObject.SetActive(false);
+        bottomTrigger.gameObject.SetActive(true);
+        FadeOut(bottomRoomSprites, BottomRoom);
+        FadeIn(topRoomSprites, TopRoom);
+    }
+    void playerEnteredBottom(object sender, EventArgsTriggererInfo args)
+    {
+        topTrigger.gameObject.SetActive(true);
+        bottomTrigger.gameObject.SetActive(false);
+        FadeOut(topRoomSprites, TopRoom);
+        FadeIn(bottomRoomSprites, BottomRoom);
+    }
+
+    void FadeOut(SpriteRenderer[] sprites, GameObject Room)
+    {
+        foreach (SpriteRenderer sprite in sprites)
+        {
+            StartCoroutine(FadeOutSprite(sprite,Room));
+        }
+    }
+    IEnumerator FadeOutSprite(SpriteRenderer sprite, GameObject room)
+    {
+        float timer = 0;
+        while(timer < TransitionTime)
+        {
+            timer += Time.deltaTime;
+            float opacity = 1- (1 / TransitionTime * timer);
+            sprite.color = new Color (1,1,1,opacity);
+            yield return null;
+        }
+        room.SetActive(false);
+    }
+
+    void FadeIn(SpriteRenderer[] sprites, GameObject Room)
+    {
+        Room.SetActive(true);
+        foreach (SpriteRenderer sprite in sprites)
+        {
+            StartCoroutine(FadeInSprites(sprite, Room));
+        }
+    }
+    IEnumerator FadeInSprites(SpriteRenderer sprite, GameObject room)
+    {
+        float timer = 0;
+        while (timer < TransitionTime)
+        {
+            timer += Time.deltaTime;
+            float opacity =  1 / TransitionTime * timer;
+            sprite.color = new Color(1, 1, 1, opacity);
+            yield return null;
+        }
+        
+    }
+}
