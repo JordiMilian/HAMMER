3881c1818f6adb9875d6e7e047b2fb3914c2d463
JordiMilian <51137598+JordiMilian@users.noreply.github.com>
2024-04-25 19:27:36 +0200

    audioplayer

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class AudioPlayer : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+    Dictionary<string,AudioClip> sfxDictionary = new Dictionary<string, AudioClip>();
+
+    [SerializeField] List<AudioClip> audioClipsList = new List<AudioClip>();
+
+
+    public static AudioPlayer Instance;
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        foreach (AudioClip Clip in audioClipsList)
+        {
+            string clipName = System.IO.Path.GetFileNameWithoutExtension(AssetDatabase.GetAssetPath(Clip)); //Get the name of the asset somehow
+            Debug.Log(Clip.name);
+            sfxDictionary.Add(clipName, Clip);
+        }
+        
+    }
+    public void playSFX(string key)
+    {
+        if(sfxDictionary.ContainsKey(key)) 
+        {
+            audioSource.clip = sfxDictionary[key];
+            audioSource.Play();
+            return;
+        }
+        Debug.LogWarning("Couldn't find an audio clip named: " + key);
+    }
+}
