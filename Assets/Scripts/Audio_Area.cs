4163f177bf8cbad32fa398458f73e7afcc955abc
JordiMilian <51137598+JordiMilian@users.noreply.github.com>
2024-05-02 20:05:39 +0200

    music area and tied dialoguer

---
+++ Assets/Scripts/Audio_Area.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Audio_Area : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+    Coroutine CurrentFade;
+    float BaseVolume;
+    [SerializeField] float FadesSeconds;
+    [SerializeField] bool isStartingArea;
+    private void Awake()
+    {
+        BaseVolume = audioSource.volume;
+    }
+    private void Start()
+    {
+        if (isStartingArea) { FadeIn(0.5f); }
+        else { FadeOut(0); }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("What?");
+        if(collision.CompareTag(TagsCollection.Player_SinglePointCollider))
+        {
+            Debug.Log("player in");
+            if(CurrentFade != null) { StopCoroutine(CurrentFade); }
+            CurrentFade = StartCoroutine( FadeIn(FadesSeconds));
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag(TagsCollection.Player_SinglePointCollider))
+        {
+            FadeOut(FadesSeconds);
+            if (CurrentFade != null) { StopCoroutine(CurrentFade); }
+            CurrentFade = StartCoroutine(FadeOut(FadesSeconds));
+        }
+    }
+    IEnumerator FadeOut(float time)
+    { 
+
+        Debug.Log("FadeIn)");
+         float timer = 0;
+        float startingVolume = audioSource.volume;
+        while (timer < time)
+        {
+            timer += Time.deltaTime;
+            float newVolume = Mathf.Lerp(startingVolume, 0, timer/time);
+            audioSource.volume = newVolume;
+            yield return null;
+        }
+        audioSource.volume = 0;
+        audioSource.Stop();
+    }
+
+    IEnumerator FadeIn(float time)
+    {
+        Debug.Log("FadeIn)");
+        audioSource.Play();
+        float timer = 0;
+        while (timer < time)
+        {
+            timer += Time.deltaTime;
+            float newVolume = Mathf.Lerp(0, BaseVolume, timer / time);
+            audioSource.volume = newVolume;
+            yield return null;
+        }
+        audioSource.volume = BaseVolume;
+       
+    }
+}
--- Assets/Scripts/Enemy/TiedEnemy/TiedEnemy_StateMachine.cs
+++ Assets/Scripts/Enemy/TiedEnemy/TiedEnemy_StateMachine.cs
@@ -7,12 +7,14 @@ public class TiedEnemy_StateMachine : Generic_StateMachine
     [SerializeField] SpriteRenderer HeadSprite;
     [SerializeField] List<SpriteRenderer> BodySprites;
     [SerializeField] Enemy_VFXManager vfxManager;
+    [SerializeField] GameObject dialoguer;
     public override void OnDeathState(object sender, Generic_EventSystem.DeadCharacterInfo args)
     {
         CurrentState = States.Dead;
         HeadSprite.enabled = false;
         vfxManager.groundBloodIntensity = 0.4f;
         GameObject.Find(TagsCollection.MainCharacter).GetComponent<Player_HealthSystem>().RestoreAllHealth();
+        dialoguer.SetActive(false);
     }
     public void ShowBodies() //Go to Respawner Manager
     {
