7b7e89e75ffc5eacfd7a57ef16cca51d1146e252
JordiMilian <51137598+JordiMilian@users.noreply.github.com>
2025-01-19 17:07:46 +0100

    chasing projectiles spawner missing script

--- Assets/Scripts/Enemy/ChasingProjectile/ChasingProjectile_Spawner.cs
+++ Assets/Scripts/Enemy/ChasingProjectile/ChasingProjectile_Spawner.cs
@@ -5,46 +5,59 @@ using UnityEngine;
 
 public class ChasingProjectile_Spawner : MonoBehaviour
 {
-    [SerializeField] Transform SpawningOriginPos;
+    [SerializeField] Transform SpawningOriginTf;
     [SerializeField] GameObject ChaserController_Prefab;
-  
+
     [Header("Spread")]
     [SerializeField] bool SpreadSpawnTrigger;
     [SerializeField] int amountToSpread;
+    [SerializeField] float angleDegToSpread;
     [SerializeField] float DistanceFromCenterOnSpawn;
     [SerializeField] float delayBetweenProjectiles;
-    [SerializeField] float angleBetweenProjectiles;
 
 
     private void Update()
     {
-        if(SpreadSpawnTrigger || Input.GetKeyDown(KeyCode.O))
+        if (SpreadSpawnTrigger || Input.GetKeyDown(KeyCode.O))
         {
-            StartCoroutine(SpreadSpawnCoroutine(amountToSpread));
+            EV_Chaser_SpreadSpawn(amountToSpread);
             SpreadSpawnTrigger = false;
         }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            EV_Chaser_SingleSpawn();
+        }
     }
-    IEnumerator SpreadSpawnCoroutine(int amount) //Decide amount from here millor
+    public void EV_Chaser_SingleSpawn()
     {
         Transform playerTf = GlobalPlayerReferences.Instance.playerTf;
-        Vector2 directionToPlayer = (playerTf.position - transform.position).normalized;
-        float totalAngle = (amount) * angleBetweenProjectiles;
-        Vector2[] SpreadDirections = UsefullMethods.GetSpreadDirectionsFromCenter(directionToPlayer, amountToSpread, Mathf.Deg2Rad * totalAngle);
 
-        List<ChasingProjectile_Controller> chasersList = new List<ChasingProjectile_Controller>();
-        for (int p = 0; p < amountToSpread; p++)
+        GameObject newChaserGO = Instantiate(ChaserController_Prefab, SpawningOriginTf.position, Quaternion.identity);
+        ChasingProjectile_Controller controller = newChaserGO.GetComponent<ChasingProjectile_Controller>();
+        Vector2 directionToPlayer = (playerTf.position - SpawningOriginTf.position).normalized;
+        controller.SpawnDelayAndChase(directionToPlayer, playerTf, transform);
+
+    }
+    public void EV_Chaser_SpreadSpawn(int amount)
+    {
+        StartCoroutine(SpreadSpawnCoroutine(amount));
+
+        //
+        IEnumerator SpreadSpawnCoroutine(int amount) //Decide amount from here millor
         {
-            Vector2 newPosition = (Vector2)transform.position + (SpreadDirections[p] * DistanceFromCenterOnSpawn);
-            GameObject newChaserGO = Instantiate(ChaserController_Prefab, newPosition, Quaternion.identity);
-            newChaserGO.transform.up = SpreadDirections[p];
-            chasersList.Add(newChaserGO.GetComponent<ChasingProjectile_Controller>());
-            yield return new WaitForSeconds(delayBetweenProjectiles);
-        }
-        //yield return new WaitForSeconds(1);
-        for (int c = 0; c < amountToSpread; c++)
-        {
-            chasersList[c].startChasing(playerTf);
-            yield return new WaitForSeconds(delayBetweenProjectiles);
+            Transform playerTf = GlobalPlayerReferences.Instance.playerTf;
+            Vector2 directionToPlayer = (playerTf.position - transform.position).normalized;
+            Vector2[] SpreadDirections = UsefullMethods.GetSpreadDirectionsFromCenter(directionToPlayer, amountToSpread, Mathf.Deg2Rad * angleDegToSpread);
+
+            for (int p = 0; p < amountToSpread; p++)
+            {
+                Vector2 newPosition = (Vector2)transform.position + (SpreadDirections[p] * DistanceFromCenterOnSpawn);
+                GameObject newChaserGO = Instantiate(ChaserController_Prefab, newPosition, Quaternion.identity);
+                ChasingProjectile_Controller controller = newChaserGO.GetComponent<ChasingProjectile_Controller>();
+                controller.SpawnDelayAndChase(SpreadDirections[p], GlobalPlayerReferences.Instance.playerTf, transform);
+                yield return new WaitForSeconds(delayBetweenProjectiles);
+            }
+
         }
     }
 }
